fcb3efeff7e3b24f29acf295214ec9e92f90e7f6
Gert van Valkenhoef <g.h.m.van.valkenhoef@rug.nl>
2015-01-09 11:07:18 +0100

    Fix license in project.clj

--- project.clj
+++ project.clj
@@ -1,8 +1,8 @@
 (defproject srdr-import "0.1.0-SNAPSHOT"
   :description "FIXME: write description"
   :url "http://example.com/FIXME"
-  :license {:name "Eclipse Public License"
-            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :license {:name "GNU GPL-3"
+            :url "https://www.gnu.org/licenses/gpl-3.0.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [korma "0.4.0"]
                  [mysql/mysql-connector-java "5.1.33"]
